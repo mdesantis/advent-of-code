2e1c863f3eee3b69b129c6a35795e9008e83cb5a
Maurizio De Santis <desantis.maurizio@gmail.com>
2024-12-13 20:24:33 +0100

    Refactoring

--- solutions/ruby/2024/09/main.rb
+++ solutions/ruby/2024/09/main.rb
@@ -6,9 +6,9 @@ require_relative 'lib/file_blocks_compacting'
 INPUT = File.read File.join(__dir__, '../../../../inputs/2024/09/input')
 
 result = FileBlocksCompacting.new(INPUT).filesystem_checksum_after_file_blocks_compacting
-puts "Filesystem checksum after file blocks compacting: #{result}"
+puts "[2024/09/1] #{result}"
 
 result = FileBlocksCompactingDealingWithFileSystemFragmentation
   .new(INPUT)
   .filesystem_checksum_after_file_blocks_compacting
-puts "Filesystem checksum after file blocks compacting dealing with filesystem fragmentation: #{result}"
+puts "[2024/09/2] #{result}"
