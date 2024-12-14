c8a23f54b29091b57135055a5c4389c4d5b7998d
Maurizio De Santis <desantis.maurizio@gmail.com>
2024-12-14 19:15:42 +0100

    Refactoring

--- solutions/ruby/2022/02/lib/rock_paper_scissors.rb
+++ solutions/ruby/2022/02/lib/rock_paper_scissors.rb
@@ -1,69 +1,71 @@
 # frozen_string_literal: true
 
-module RockPaperScissors
-  module Shape
-    ROCK = :rock
-    PAPER = :paper
-    SCISSORS = :scissors
-  end
-
-  module Outcome
-    WIN = :won
-    LOOSE = :lost
-    DRAW = :draw
-  end
-
-  class Round
-    attr_reader :opponent, :player
-
-    SHAPE_SCORES = {
-      Shape::ROCK => 1,
-      Shape::PAPER => 2,
-      Shape::SCISSORS => 3
-    }.freeze
-
-    OUTCOMES = {
-      [Shape::SCISSORS, Shape::ROCK] => Outcome::WIN,
-      [Shape::PAPER, Shape::SCISSORS] => Outcome::WIN,
-      [Shape::ROCK, Shape::PAPER] => Outcome::WIN,
-      [Shape::ROCK, Shape::SCISSORS] => Outcome::LOOSE,
-      [Shape::SCISSORS, Shape::PAPER] => Outcome::LOOSE,
-      [Shape::PAPER, Shape::ROCK] => Outcome::LOOSE
-    }.freeze
-
-    OUTCOME_SCORES = {
-      Outcome::WIN => 6,
-      Outcome::DRAW => 3,
-      Outcome::LOOSE => 0
-    }.freeze
-
-    def initialize(opponent, player)
-      @opponent = opponent
-      @player = player
+module Solutions202202
+  module RockPaperScissors
+    module Shape
+      ROCK = :rock
+      PAPER = :paper
+      SCISSORS = :scissors
     end
 
-    def outcome_score
-      OUTCOME_SCORES[outcome]
+    module Outcome
+      WIN = :won
+      LOOSE = :lost
+      DRAW = :draw
     end
 
-    def score
-      shape_score + outcome_score
+    class Round
+      attr_reader :opponent, :player
+
+      SHAPE_SCORES = {
+        Shape::ROCK => 1,
+        Shape::PAPER => 2,
+        Shape::SCISSORS => 3
+      }.freeze
+
+      OUTCOMES = {
+        [Shape::SCISSORS, Shape::ROCK] => Outcome::WIN,
+        [Shape::PAPER, Shape::SCISSORS] => Outcome::WIN,
+        [Shape::ROCK, Shape::PAPER] => Outcome::WIN,
+        [Shape::ROCK, Shape::SCISSORS] => Outcome::LOOSE,
+        [Shape::SCISSORS, Shape::PAPER] => Outcome::LOOSE,
+        [Shape::PAPER, Shape::ROCK] => Outcome::LOOSE
+      }.freeze
+
+      OUTCOME_SCORES = {
+        Outcome::WIN => 6,
+        Outcome::DRAW => 3,
+        Outcome::LOOSE => 0
+      }.freeze
+
+      def initialize(opponent, player)
+        @opponent = opponent
+        @player = player
+      end
+
+      def outcome_score
+        OUTCOME_SCORES[outcome]
+      end
+
+      def score
+        shape_score + outcome_score
+      end
     end
-  end
 
-  class Game
-    def self.from_input(input)
-      new(input.split("\n").map { |input_line| round_class.from_input_line(input_line) })
-    end
+    class Game
+      def self.from_input(input)
+        new(input.split("\n").map { |input_line| round_class.from_input_line(input_line) })
+      end
 
-    attr_reader :rounds
+      attr_reader :rounds
 
-    def initialize(rounds)
-      @rounds = rounds
-    end
+      def initialize(rounds)
+        @rounds = rounds
+      end
 
-    def score
-      rounds.sum(&:score)
+      def score
+        rounds.sum(&:score)
+      end
     end
   end
 end
--- solutions/ruby/2022/02/lib/rock_paper_scissors/part_2.rb
+++ solutions/ruby/2022/02/lib/rock_paper_scissors/part_2.rb
@@ -2,81 +2,84 @@
 
 require_relative '../rock_paper_scissors'
 
-module RockPaperScissors
-  module Part2
-    class Opponent
-      SHAPES_BY_INPUT = { 'A' => Shape::ROCK, 'B' => Shape::PAPER, 'C' => Shape::SCISSORS }.freeze
+module Solutions202202
+  module RockPaperScissors
+    module Part2
+      class Opponent
+        SHAPES_BY_INPUT = { 'A' => Shape::ROCK, 'B' => Shape::PAPER, 'C' => Shape::SCISSORS }.freeze
 
-      attr_reader :shape
+        attr_reader :shape
 
-      def self.from_shape_input(shape_input)
-        new SHAPES_BY_INPUT[shape_input]
+        def self.from_shape_input(shape_input)
+          new SHAPES_BY_INPUT[shape_input]
+        end
+
+        def initialize(shape)
+          @shape = shape
+        end
       end
 
-      def initialize(shape)
-        @shape = shape
-      end
-    end
+      class Player
+        EXPECTED_OUTCOMES_BY_INPUT = { 'X' => Outcome::LOOSE, 'Y' => Outcome::DRAW, 'Z' => Outcome::WIN }.freeze
 
-    class Player
-      EXPECTED_OUTCOMES_BY_INPUT = { 'X' => Outcome::LOOSE, 'Y' => Outcome::DRAW, 'Z' => Outcome::WIN }.freeze
+        attr_reader :expected_outcome
 
-      attr_reader :expected_outcome
+        def self.from_expected_outcome_input(expected_outcome_input)
+          new EXPECTED_OUTCOMES_BY_INPUT[expected_outcome_input]
+        end
 
-      def self.from_expected_outcome_input(expected_outcome_input)
-        new EXPECTED_OUTCOMES_BY_INPUT[expected_outcome_input]
-      end
-
-      def initialize(expected_outcome)
-        @expected_outcome = expected_outcome
-      end
-    end
-
-    class Round < Round
-      GUESSED_PLAYER_SHAPES = {
-        [Shape::ROCK, Outcome::WIN] => Shape::PAPER,
-        [Shape::PAPER, Outcome::WIN] => Shape::SCISSORS,
-        [Shape::SCISSORS, Outcome::WIN] => Shape::ROCK,
-
-        [Shape::ROCK, Outcome::DRAW] => Shape::ROCK,
-        [Shape::PAPER, Outcome::DRAW] => Shape::PAPER,
-        [Shape::SCISSORS, Outcome::DRAW] => Shape::SCISSORS,
-
-        [Shape::ROCK, Outcome::LOOSE] => Shape::SCISSORS,
-        [Shape::PAPER, Outcome::LOOSE] => Shape::ROCK,
-        [Shape::SCISSORS, Outcome::LOOSE] => Shape::PAPER
-      }.freeze
-
-      attr_reader :guessed_player_shape
-
-      def self.from_input_line(input_line)
-        opponent_shape_input, expected_outcome_input = input_line.split
-        new Opponent.from_shape_input(opponent_shape_input), Player.from_expected_outcome_input(expected_outcome_input)
+        def initialize(expected_outcome)
+          @expected_outcome = expected_outcome
+        end
       end
 
-      def initialize(opponent, player)
-        super
-        @guessed_player_shape = guess_player_shape
+      class Round < Round
+        GUESSED_PLAYER_SHAPES = {
+          [Shape::ROCK, Outcome::WIN] => Shape::PAPER,
+          [Shape::PAPER, Outcome::WIN] => Shape::SCISSORS,
+          [Shape::SCISSORS, Outcome::WIN] => Shape::ROCK,
+
+          [Shape::ROCK, Outcome::DRAW] => Shape::ROCK,
+          [Shape::PAPER, Outcome::DRAW] => Shape::PAPER,
+          [Shape::SCISSORS, Outcome::DRAW] => Shape::SCISSORS,
+
+          [Shape::ROCK, Outcome::LOOSE] => Shape::SCISSORS,
+          [Shape::PAPER, Outcome::LOOSE] => Shape::ROCK,
+          [Shape::SCISSORS, Outcome::LOOSE] => Shape::PAPER
+        }.freeze
+
+        attr_reader :guessed_player_shape
+
+        def self.from_input_line(input_line)
+          opponent_shape_input, expected_outcome_input = input_line.split
+          new Opponent.from_shape_input(opponent_shape_input),
+              Player.from_expected_outcome_input(expected_outcome_input)
+        end
+
+        def initialize(opponent, player)
+          super
+          @guessed_player_shape = guess_player_shape
+        end
+
+        def guess_player_shape
+          GUESSED_PLAYER_SHAPES[[opponent.shape, player.expected_outcome]]
+        end
+
+        def shape_score
+          SHAPE_SCORES[guessed_player_shape]
+        end
+
+        def outcome
+          return Outcome::DRAW if opponent.shape == guessed_player_shape
+
+          OUTCOMES[[opponent.shape, guessed_player_shape]]
+        end
       end
 
-      def guess_player_shape
-        GUESSED_PLAYER_SHAPES[[opponent.shape, player.expected_outcome]]
-      end
-
-      def shape_score
-        SHAPE_SCORES[guessed_player_shape]
-      end
-
-      def outcome
-        return Outcome::DRAW if opponent.shape == guessed_player_shape
-
-        OUTCOMES[[opponent.shape, guessed_player_shape]]
-      end
-    end
-
-    class Game < Game
-      def self.round_class
-        Round
+      class Game < Game
+        def self.round_class
+          Round
+        end
       end
     end
   end
